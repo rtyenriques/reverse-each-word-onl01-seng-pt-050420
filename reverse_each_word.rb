4704fe786c1ec841644c64509f7a60e44aface3f
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-13 01:47:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,11 +11,10 @@ def reverse_each_word(string)
   array = string.split(" ") 
   test_array = []
   array.collect do|string|
-    test_array << string.reverse 
+    test_array << string.reverse
   end
   test_array.join(" ")
 end
 
 
 reverse_each_word("hi there, and how are you?")
-
